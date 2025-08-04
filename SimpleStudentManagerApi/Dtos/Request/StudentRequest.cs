92432ebe9a678ba13c89781f180e4d4309074855
mohammadsofan <mohammadsof72@gmail.com>
2025-08-04 12:39:20 +0300

    feat(student): add a Student Request Dto with Name, Age, Major fields

---
+++ SimpleStudentManagerApi/Dtos/Request/StudentRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleStudentManagerApi.Dtos.Request
+{
+    public class StudentRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Major { get; set; } = string.Empty;
+    }
+}
